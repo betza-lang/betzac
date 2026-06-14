a2e4b7ab25360de23f60c9c5643df11dfae08fc5
Thierry-git <thierry.poulin000@gmail.com>
2026-06-14 15:02:59 -0400

    feat(pipeline): preparatory work to chain pipeline steps one into the next + readFile error handling

--- app/Betzac/Main.hs
+++ app/Betzac/Main.hs
@@ -2,24 +2,45 @@ module Main (main) where
 
 import Betzac.Lexer.Core (LexError (..))
 import Betzac.Pipeline (PipelineResult (..), fromScratch)
+import Control.Exception (IOException, try)
+import Data.Text (Text)
 import Data.Text.IO (readFile)
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
 import Prelude hiding (readFile)
 
+showLexResults :: PipelineResult -> IO ()
+showLexResults p = case lexResult p of
+    Nothing -> mempty
+    Just (Left (LexError pos)) -> do
+        hPutStrLn stderr $ "Lex error at position " ++ show pos
+        exitFailure
+    Just (Right tokens) -> mapM_ print tokens
+
+showParseResults :: PipelineResult -> IO ()
+showParseResults = mempty
+
+showAnalysis :: PipelineResult -> IO ()
+showAnalysis = mempty
+
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         [path] -> do
-            src <- readFile path
-            case lexResult (fromScratch src) of
-                Nothing -> pure ()
-                Just (Left (LexError pos)) -> do
-                    hPutStrLn stderr $ "Lex error at position " ++ show pos
+            f <- try (readFile path) :: IO (Either IOException Text)
+            case f of
+                Left err -> do
+                    hPutStrLn stderr $ "Could not read file"
+                    hPutStrLn stderr $ show err
                     exitFailure
-                Just (Right tokens) -> mapM_ print tokens
+                Right src -> case fromScratch src of
+                    Left err -> hPutStrLn stderr $ "Fatal pipeline error: " ++ show err
+                    Right results -> do
+                        showLexResults results
+                        showParseResults results
+                        showAnalysis results
         _ -> do
             hPutStrLn stderr "Usage: betzac <file>"
             exitFailure
--- src/Betzac/Pipeline.hs
+++ src/Betzac/Pipeline.hs
@@ -2,30 +2,53 @@ module Betzac.Pipeline (
     fromScratch,
     updatePipeline,
     PipelineResult (..),
+    PipelineError (..),
 ) where
 
 import Betzac.Lexer.Core (LexError (..), runLexer)
-import Betzac.Lexer.Lexer (lexSource)
+import qualified Betzac.Lexer.Lexer as Lexer (lexSource)
 import Betzac.Token (Token)
+import Control.Monad.Trans.State.Strict (StateT, execStateT, gets, modify)
 import Data.Text (Text, unpack)
+import Prelude hiding (length)
 
-updatePipeline :: PipelineResult -> Text -> PipelineResult
-updatePipeline _ = fromScratch
-
-fromScratch :: Text -> PipelineResult
-fromScratch srcTxt =
-    PipelineResult
-        { sourceText = srcTxt
-        , lexResult = Just $ case runLexer lexSource s of
-            Left err -> Left err
-            Right (tokens, _, []) -> Right tokens
-            Right (_, pos, _) -> Left $ LexError pos
-        }
-  where
-    s = unpack srcTxt
+type Pipeline a = StateT PipelineResult (Either PipelineError) a
 
 data PipelineResult = PipelineResult
     { sourceText :: Text
     , lexResult :: Maybe (Either LexError [Token])
     -- , astResult :: Maybe (Either ... BetzaExpr)
     }
+    deriving (Show)
+
+emptyResult :: Text -> PipelineResult
+emptyResult src =
+    PipelineResult
+        { sourceText = src
+        , lexResult = Nothing
+        }
+
+-- TODO
+-- ParseFailed ParseError
+-- AnalysisFailed AnalysisError
+data PipelineError
+    = SystemError String
+    deriving (Show)
+
+pipeline :: Pipeline ()
+pipeline = do
+    lexSource
+
+lexSource :: Pipeline ()
+lexSource = do
+    src <- gets $ unpack . sourceText
+    modify $ \r ->
+        r
+            { lexResult = Just $ (\(tokens, _, _) -> tokens) <$> runLexer Lexer.lexSource src
+            }
+
+updatePipeline :: PipelineResult -> Text -> Either PipelineError PipelineResult
+updatePipeline _ = fromScratch
+
+fromScratch :: Text -> Either PipelineError PipelineResult
+fromScratch src = execStateT pipeline (emptyResult src)
