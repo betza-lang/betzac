f7be0a84737695984b67136b1a6e315503a47944
Thierry-git <thierry.poulin000@gmail.com>
2026-06-14 15:22:51 -0400

    refactor(stream mutator): essentially factored out core into an abstraction that will apply to parsing

--- src/Betzac/Lexer/Core.hs
+++ src/Betzac/Lexer/Core.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DerivingVia #-}
-
 module Betzac.Lexer.Core (
     module Control.Applicative,
     Lexer,
@@ -16,58 +14,19 @@ module Betzac.Lexer.Core (
     singleton,
 ) where
 
+-- for peek
+import Betzac.StreamMutator
 import Control.Applicative (Alternative (..))
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.State.Strict (StateT (..), get, gets, put, runStateT)
-import Data.Maybe (listToMaybe) -- for peek
 
-newtype Lexer a = Lexer (StateT (Int, String) (Either LexError) a)
-    deriving
-        (Functor, Applicative, Monad)
-        via StateT (Int, String) (Either LexError)
+type Lexer = StreamMutator LexError Char
 
 runLexer :: Lexer a -> String -> Either LexError (a, Int, String)
-runLexer (Lexer l) s = (\(a, (n, s')) -> (a, n, s')) <$> runStateT l (0, s)
+runLexer = runMutator
 
 data LexError = LexError Int deriving (Eq, Show)
 
-instance Alternative Lexer where
-    empty = Lexer $ gets fst >>= lift . Left . LexError
-    Lexer l <|> Lexer r = Lexer $ StateT $ \s -> either (const $ runStateT r s) Right (runStateT l s)
-    some p = p >>= \x -> (x :) <$> many p
-
-peek :: Lexer (Maybe Char)
-peek = Lexer $ gets $ listToMaybe . snd
-
-sat :: (Char -> Bool) -> Lexer Char
-sat p = do
-    (n, s) <- Lexer get
-    case s of
-        [] -> empty
-        (c : cs) -> if p c then Lexer $ put (n + 1, cs) >> return c else empty
-
-advance :: Lexer Char
-advance = sat $ const True
-
-failOn :: (Char -> Bool) -> Lexer ()
-failOn p = do
-    mc <- peek
-    case mc of
-        Just c | p c -> empty
-        _ -> return ()
+instance StreamError LexError where
+    errorAt = LexError
 
 char :: Char -> Lexer Char
-char c = sat (== c)
-
-oneOf :: String -> Lexer Char
-oneOf s = sat $ (`elem` s)
-
-noneOf :: String -> Lexer Char
-noneOf s = sat (`notElem` s)
-
-match :: String -> Lexer String
-match [] = pure []
-match (c : cs) = (:) <$> (char c) <*> (match cs)
-
-singleton :: Lexer a -> Lexer [a]
-singleton = fmap (: [])
+char = matchOne
---
+++ src/Betzac/StreamMutator.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE DerivingVia #-}
+
+module Betzac.StreamMutator (
+    StreamMutator (..),
+    runMutator,
+    module Control.Applicative,
+    StreamError (..),
+    peek,
+    sat,
+    advance,
+    failOn,
+    matchOne,
+    match,
+    oneOf,
+    noneOf,
+    singleton,
+) where
+
+-- for peek
+import Control.Applicative (Alternative (..))
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.State.Strict (StateT (..), get, gets, put)
+import Data.Maybe (listToMaybe)
+
+newtype StreamMutator e s a = StreamMutator (StateT (Int, [s]) (Either e) a)
+    deriving
+        (Functor, Applicative, Monad)
+        via StateT (Int, [s]) (Either e)
+
+runMutator :: StreamMutator e s a -> [s] -> Either e (a, Int, [s])
+runMutator (StreamMutator m) ss = (\(a, (n, ss')) -> (a, n, ss')) <$> runStateT m (0, ss)
+
+class StreamError e where
+    errorAt :: Int -> e
+
+instance (StreamError e) => Alternative (StreamMutator e s) where
+    empty = StreamMutator $ gets fst >>= lift . Left . errorAt
+    StreamMutator l <|> StreamMutator r = StreamMutator $ StateT $ \s -> either (const $ runStateT r s) Right (runStateT l s)
+    some p = p >>= \x -> (x :) <$> many p
+
+peek :: StreamMutator e s (Maybe s)
+peek = StreamMutator $ gets $ listToMaybe . snd
+
+sat :: (StreamError e) => (s -> Bool) -> StreamMutator e s s
+sat p = do
+    (n, s) <- StreamMutator get
+    case s of
+        [] -> empty
+        (c : cs) -> if p c then StreamMutator $ put (n + 1, cs) >> return c else empty
+
+advance :: (StreamError e) => StreamMutator e s s
+advance = sat $ const True
+
+failOn :: (StreamError e) => (s -> Bool) -> StreamMutator e s ()
+failOn p = do
+    mc <- peek
+    case mc of
+        Just c | p c -> empty
+        _ -> return ()
+
+matchOne :: (Eq s, StreamError e) => s -> StreamMutator e s s
+matchOne s = sat (== s)
+
+match :: (Eq s, StreamError e) => [s] -> StreamMutator e s [s]
+match = mapM matchOne
+
+oneOf :: (Eq s, StreamError e) => [s] -> StreamMutator e s s
+oneOf ss = sat $ (`elem` ss)
+
+noneOf :: (Eq s, StreamError e) => [s] -> StreamMutator e s s
+noneOf ss = sat (`notElem` ss)
+
+singleton :: StreamMutator e s s -> StreamMutator e s [s]
+singleton = fmap (: [])
